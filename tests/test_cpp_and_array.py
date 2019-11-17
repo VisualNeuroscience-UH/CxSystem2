c65a09a25f6031e7aa6d81359ad39aa8508a2950
Vafa Andalibi <vafa.andalibi@gmail.com>
2019-11-17 12:55:01 -0500

    code cleaninng for cpp test

--- tests/test_cpp_and_array.py
+++ tests/test_cpp_and_array.py
@@ -45,7 +45,7 @@ def cxsystem_run_fixture2():
 	yield  # Run the tests here
 	
 	#Executing teardown code. Connections are already gone if run with basic test
-	# shutil.rmtree(simulation_path.as_posix())
+	shutil.rmtree(simulation_path.as_posix())
 
 
 @pytest.fixture(scope='module')
