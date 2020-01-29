7eef96f9297de25a0528e5f4c36ce90163122bda
Vafa Andalibi <vafa.andalibi@gmail.com>
2020-01-28 21:07:55 -0500

    BUI workaround to prevent messed up button orders

--- cxsystem2/bui/cx_bui/static/scripts/auth.js
+++ cxsystem2/bui/cx_bui/static/scripts/auth.js
@@ -26,7 +26,7 @@ function authenticate(callback) {
             method: 'GET',
             success: function (data) {
                 hbp_auth_response = data;
-                document.getElementById('welcome').innerHTML = "Welcome " + data.givenName + "  <img id=\"imagebox\" width=\"30\" height=\"30\" style='margin-left:5px;'/>";
+                document.getElementById('welcome').innerHTML = "Hello " + data.givenName + "  <img id=\"imagebox\" width=\"30\" height=\"30\" style='margin-left:5px;'/>";
                 document.getElementById('imagebox').src = data.picture;
 
                 // console.log(data);
