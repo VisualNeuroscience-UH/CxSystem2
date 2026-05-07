9d73ee2da66a7cdc0747c5e916c496319722ca4d
simo <simo.vanni@helsinki.fi>
2026-05-07 10:50:26 +0300

    main __init__.py: check between installed package version and current pyproject.toml version number. Raise error and guide reinstall if not a match

--- cxsystem2/__init__.py
+++ cxsystem2/__init__.py
@@ -1,3 +1,18 @@
 from importlib.metadata import version
+from pathlib import Path
+
+import tomllib
 
 __version__ = version("cxsystem2")
+
+
+def get_version_from_pyproject():
+    pyproject_path = Path(__file__).parent.parent / "pyproject.toml"
+    with open(pyproject_path, "rb") as f:
+        data = tomllib.load(f)
+    return data["tool"]["poetry"]["version"]
+
+if __version__ != get_version_from_pyproject():
+    raise ValueError(
+        f"Version mismatch: __version__ is {__version__} but pyproject.toml version is {get_version_from_pyproject()}. Run poetry install at your repo root to update to the version in pyproject.toml."
+    )
